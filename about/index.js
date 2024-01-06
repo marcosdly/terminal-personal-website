01c02a905de24d7ea7c10c643f55fa25d3a50506
Marcos Delay <dev.marcosdly@gmail.com>
2024-01-06 01:34:16 -0300

    Figured it out how scroll with overflow hidden works

--- about/index.js
+++ about/index.js
@@ -1,5 +1,16 @@
 "use strict";
 
+/**
+ *
+ * @param {string} str
+ * @returns {Number?}
+ */
+function firstSetOfNumbersFromString(str) {
+  const nums = str.match(/-?[0-9]+/);
+  if (!nums) return null;
+  return Number(nums);
+}
+
 const photoSelectElements = document.querySelectorAll("ol.photos-carrousel > .select");
 const photoListItems = document.querySelectorAll("ol.photos-carrousel > li");
 
@@ -25,39 +36,62 @@ const articleMain = document.querySelector("article > main");
 const articleScrollContainer = document.querySelector("article > main > .scroll-container");
 const customScrollbar = document.querySelector("body > .scrollbar");
 const customScrollbarIndicator = document.querySelector("body > .scrollbar > .actual-bar");
+const scrollCeiling = document.querySelector("article > main > .scroll-ceiling");
 
-function customScrollbarCalculate() {
-  if (!customScrollbar || !customScrollbarIndicator || !articleMain || !articleScrollContainer)
-    return;
-  else if (articleScrollContainer.scrollHeight <= articleScrollContainer.offsetHeight) return;
-  else if (articleScrollContainer.scrollTop === 0) {
+/**
+ *
+ * @param {WheelEvent} event
+ * @returns {void}
+ */
+function customScrollbarCalculate(event) {
+  if (!customScrollbar || !customScrollbarIndicator || !articleMain || !scrollCeiling) return;
+
+  // No need to check for null. There's a style tag set to '0px' in HTML.
+  let scrollerOffset = firstSetOfNumbersFromString(scrollCeiling.style.marginTop);
+  const maxOffsetRange =
+    articleMain.scrollHeight +
+    (scrollerOffset < 0 ? -scrollerOffset : 0) -
+    articleMain.offsetHeight;
+  if (maxOffsetRange <= 0) {
+    scrollCeiling.style.marginTop = "0px";
+  } else if (event) {
+    (function () {
+      const afterScrolling = maxOffsetRange - event.deltaY;
+      if (event.deltaY === 0 || scrollerOffset === maxOffsetRange) return;
+      else if (event.deltaY < 0 && scrollerOffset - event.deltaY < 0)
+        scrollCeiling.style.marginTop = `${scrollerOffset - event.deltaY}px`;
+      else if (event.deltaY < 0 && scrollerOffset - event.deltaY >= 0)
+        scrollCeiling.style.marginTop = "0px";
+      else if (afterScrolling <= 0) scrollCeiling.style.marginTop = `${-maxOffsetRange}px`;
+      else if (Math.abs(scrollerOffset - event.deltaY) >= maxOffsetRange)
+        scrollCeiling.style.marginTop = `${-maxOffsetRange}px`;
+      else scrollCeiling.style.marginTop = `${scrollerOffset - event.deltaY}px`;
+    })();
+    scrollerOffset = firstSetOfNumbersFromString(scrollCeiling.style.marginTop);
+  }
+
+  if (scrollerOffset === 0) {
     customScrollbarIndicator.style.marginTop = "0px";
     return;
   }
 
-  const maxOffsetRange = articleScrollContainer.scrollHeight - articleScrollContainer.offsetHeight;
-  const percentageScrolled = articleScrollContainer.scrollTop / maxOffsetRange;
+  const percentageScrolled = Math.abs(scrollerOffset) / maxOffsetRange;
   const maxOffsetBarRange = customScrollbar.offsetHeight - customScrollbarIndicator.offsetHeight;
   customScrollbarIndicator.style.marginTop = `${maxOffsetBarRange * percentageScrolled}px`;
 }
 
 function scrollbarHideResizing() {
-  if (!customScrollbar || !customScrollbarIndicator || !articleMain || !articleScrollContainer)
-    return;
+  if (!customScrollbar || !customScrollbarIndicator || !articleMain || !scrollCeiling) return;
 
   const totalBarHeight = articleMain.offsetHeight * 0.4;
   customScrollbar.style.height = `${totalBarHeight}px`;
   customScrollbarIndicator.style.height = `${
-    totalBarHeight * (articleScrollContainer.offsetHeight / articleScrollContainer.scrollHeight)
+    totalBarHeight * (articleMain.offsetHeight / articleMain.scrollHeight)
   }px`;
   customScrollbarCalculate();
-
-  const scrollbarWidth = articleScrollContainer.offsetWidth - articleScrollContainer.scrollWidth;
-  articleScrollContainer.style.width = `${articleMain.offsetWidth + scrollbarWidth}px`;
 }
 
 addEventListener("resize", scrollbarHideResizing);
-if (articleScrollContainer)
-  articleScrollContainer.addEventListener("scroll", customScrollbarCalculate);
+articleMain.addEventListener("wheel", customScrollbarCalculate);
 
 scrollbarHideResizing();

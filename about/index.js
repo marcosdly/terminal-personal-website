590a05386db8c14e248c41b6ecfd18149771ac7e
Marcos Delay <dev.marcosdly@gmail.com>
2024-01-06 03:47:01 -0300

    Un-shitty-fy scrolling implementation of overflow hidden

--- about/index.js
+++ about/index.js
@@ -1,14 +1,112 @@
 "use strict";
 
 /**
- *
  * @param {string} str
  * @returns {Number?}
  */
 function firstSetOfNumbersFromString(str) {
   const nums = str.match(/-?[0-9]+/);
-  if (!nums) return null;
+  if (nums === null) return nums;
   return Number(nums);
+}
+
+class Scroller {
+  /**
+   * @param {Element} scrollable Element that will be scrolled (container)
+   * @param {Element} scroller Element that allows the other to scroll (empty div on top)
+   * @param {Number} deltaY Scrolling offset
+   * @param {Boolean} inverted Is scroll inverted?
+   */
+  constructor(scrollable, scroller, deltaY, inverted = false) {
+    this.scrollable = scrollable;
+    this.scroller = scroller;
+    this.inverted = inverted;
+    this.delta = inverted ? -deltaY : deltaY;
+    this.scrollerOffset = firstSetOfNumbersFromString(this.scroller.style.marginTop);
+    if (this.scrollerOffset === null) {
+      this.unset();
+      this.scrollerOffset = 0;
+    }
+  }
+
+  /**
+   * @returns {Boolean} Is user scrolling up?
+   */
+  scrollingUp() {
+    return this.delta < 0;
+  }
+
+  /**
+   * @returns {Number}
+   */
+  maxOffsetRange() {
+    if (this.scrollerOffset < 0) {
+      return (
+        this.scrollable.scrollHeight + Math.abs(this.scrollerOffset) - this.scrollable.offsetHeight
+      );
+    }
+    return this.scrollable.scrollHeight - this.scrollable.offsetHeight;
+  }
+
+  /**
+   * Either an init operation in case you don't inline style already set up, or
+   * a error fallback behavior.
+   */
+  unset() {
+    this.scroller.style.marginTop = "0px";
+  }
+
+  /**
+   * ~ unused ~
+   * @returns {Boolean} Can the content be scrolled any higher?
+   */
+  canScrollUp() {
+    return this.scrollerOffset - this.delta < 0;
+  }
+
+  /**
+   * ~ unused ~
+   * @returns {Boolean} Can the content be scrolled any lower?
+   */
+  canScrollDown() {
+    return (
+      this.maxOffsetRange() - this.delta > 0 ||
+      Math.abs(this.scrollerOffset - this.delta) < this.maxOffsetRange()
+    );
+  }
+
+  /**
+   * @returns {Number} Raw, unchecked scroll amount
+   */
+  #calcScrollUnsafe() {
+    return this.scrollerOffset - this.delta;
+  }
+
+  /**
+   * @returns {Boolean} Will the next scroll put you out of bounds?
+   */
+  isNextTooMuch() {
+    if (this.scrollingUp() && this.#calcScrollUnsafe() > 0) return true;
+
+    if (!this.scrollingUp() && Math.abs(this.#calcScrollUnsafe()) >= this.maxOffsetRange())
+      return true;
+
+    return false;
+  }
+
+  /**
+   * Scroll content.
+   * @returns {void}
+   */
+  scroll() {
+    if (this.delta === 0 || this.scrollerOffset === this.maxOffsetRange()) return;
+    else if (this.scrollingUp() && this.isNextTooMuch()) this.unset();
+    else if (this.isNextTooMuch()) this.scroller.style.marginTop = `${-this.maxOffsetRange()}px`;
+    else {
+      this.scrollerOffset = this.scrollerOffset - this.delta;
+      this.scroller.style.marginTop = `${this.scrollerOffset}px`;
+    }
+  }
 }
 
 const photoSelectElements = document.querySelectorAll("ol.photos-carrousel > .select");
@@ -46,36 +144,18 @@ const scrollCeiling = document.querySelector("article > main > .scroll-ceiling")
 function customScrollbarCalculate(event) {
   if (!customScrollbar || !customScrollbarIndicator || !articleMain || !scrollCeiling) return;
 
-  // No need to check for null. There's a style tag set to '0px' in HTML.
-  let scrollerOffset = firstSetOfNumbersFromString(scrollCeiling.style.marginTop);
-  const maxOffsetRange =
-    articleMain.scrollHeight +
-    (scrollerOffset < 0 ? -scrollerOffset : 0) -
-    articleMain.offsetHeight;
-  if (maxOffsetRange <= 0) {
-    scrollCeiling.style.marginTop = "0px";
-  } else if (event) {
-    (function () {
-      const afterScrolling = maxOffsetRange - event.deltaY;
-      if (event.deltaY === 0 || scrollerOffset === maxOffsetRange) return;
-      else if (event.deltaY < 0 && scrollerOffset - event.deltaY < 0)
-        scrollCeiling.style.marginTop = `${scrollerOffset - event.deltaY}px`;
-      else if (event.deltaY < 0 && scrollerOffset - event.deltaY >= 0)
-        scrollCeiling.style.marginTop = "0px";
-      else if (afterScrolling <= 0) scrollCeiling.style.marginTop = `${-maxOffsetRange}px`;
-      else if (Math.abs(scrollerOffset - event.deltaY) >= maxOffsetRange)
-        scrollCeiling.style.marginTop = `${-maxOffsetRange}px`;
-      else scrollCeiling.style.marginTop = `${scrollerOffset - event.deltaY}px`;
-    })();
-    scrollerOffset = firstSetOfNumbersFromString(scrollCeiling.style.marginTop);
+  const scroller = new Scroller(articleMain, scrollCeiling, event?.deltaY ?? 0);
+
+  if (event) {
+    scroller.scroll();
   }
 
-  if (scrollerOffset === 0) {
+  if (scroller.scrollerOffset === 0) {
     customScrollbarIndicator.style.marginTop = "0px";
     return;
   }
 
-  const percentageScrolled = Math.abs(scrollerOffset) / maxOffsetRange;
+  const percentageScrolled = Math.abs(scroller.scrollerOffset) / scroller.maxOffsetRange();
   const maxOffsetBarRange = customScrollbar.offsetHeight - customScrollbarIndicator.offsetHeight;
   customScrollbarIndicator.style.marginTop = `${maxOffsetBarRange * percentageScrolled}px`;
 }

59037a3c1cc25c606eaed98fdb24bd7779996cf5
Marcos Delay <dev.marcosdly@gmail.com>
2024-01-09 10:17:59 -0300

    Fixed bad minifying config option

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     ViteMinifyPlugin({
       caseSensitive: true,
-      collapseInlineTagWhitespace: true,
+      collapseInlineTagWhitespace: false,
       collapseWhitespace: true,
       html5: true,
       keepClosingSlash: true,

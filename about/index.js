962b0e06e1383c2c0ba328200e2c1346561b21c2
Marcos Delay <dev.marcosdly@gmail.com>
2024-01-18 10:54:47 -0300

    feat(refactor,touch): Scroller class reworked
    Scroller class' structure now allows to many input methods.
    
    Implemented touch scrolling support.

--- about/index.js
+++ about/index.js
@@ -1,120 +1,7 @@
 "use strict";
 
 import { FetchImage } from "../js/firebase/storage";
-
-/**
- * @param {string} str
- * @returns {Number?}
- */
-function firstSetOfNumbersFromString(str) {
-  const nums = str.match(/-?[0-9]+/);
-  if (nums === null) return nums;
-  return Number(nums);
-}
-
-class Scroller {
-  /**
-   * @param {Element} scrollable Element that will be scrolled (container)
-   * @param {Element} scroller Element that allows the other to scroll (empty div on top)
-   * @param {Number} deltaY Scrolling offset
-   * @param {Boolean} inverted Is scroll inverted?
-   */
-  constructor(scrollable, scroller, deltaY, inverted = false) {
-    this.scrollable = scrollable;
-    this.scroller = scroller;
-    this.inverted = inverted;
-    this.delta = inverted ? -deltaY : deltaY;
-    this.scrollerOffset = firstSetOfNumbersFromString(this.scroller.style.marginTop);
-    if (this.scrollerOffset === null) {
-      this.unset();
-      this.scrollerOffset = 0;
-    }
-  }
-
-  /**
-   * @returns {Boolean} Is user scrolling up?
-   */
-  scrollingUp() {
-    return this.delta < 0;
-  }
-
-  /**
-   * @returns {Number}
-   */
-  maxOffsetRange() {
-    if (this.scrollerOffset < 0) {
-      return (
-        this.scrollable.scrollHeight + Math.abs(this.scrollerOffset) - this.scrollable.offsetHeight
-      );
-    }
-    return this.scrollable.scrollHeight - this.scrollable.offsetHeight;
-  }
-
-  /**
-   * Either an init operation in case you don't inline style already set up, or
-   * a error fallback behavior.
-   */
-  unset() {
-    this.scroller.style.marginTop = "0px";
-  }
-
-  /**
-   * ~ unused ~
-   * @returns {Boolean} Can the content be scrolled any higher?
-   */
-  canScrollUp() {
-    return this.scrollerOffset - this.delta < 0;
-  }
-
-  /**
-   * ~ unused ~
-   * @returns {Boolean} Can the content be scrolled any lower?
-   */
-  canScrollDown() {
-    return (
-      this.maxOffsetRange() - this.delta > 0 ||
-      Math.abs(this.scrollerOffset - this.delta) < this.maxOffsetRange()
-    );
-  }
-
-  /**
-   * @returns {Number} Raw, unchecked scroll amount
-   */
-  #calcScrollUnsafe() {
-    return this.scrollerOffset - this.delta;
-  }
-
-  /**
-   * @returns {Boolean} Will the next scroll put you out of bounds?
-   */
-  isNextTooMuch() {
-    if (this.scrollingUp() && this.#calcScrollUnsafe() > 0) return true;
-
-    if (!this.scrollingUp() && Math.abs(this.#calcScrollUnsafe()) >= this.maxOffsetRange())
-      return true;
-
-    return false;
-  }
-
-  /**
-   * Scroll content.
-   * @returns {void}
-   */
-  scroll() {
-    if (this.delta === 0 && this.scrollable.offsetHeight === this.scrollable.scrollHeight) {
-      this.unset();
-      return;
-    }
-
-    if (this.delta === 0 || this.scrollerOffset === this.maxOffsetRange()) return;
-    else if (this.scrollingUp() && this.isNextTooMuch()) this.unset();
-    else if (this.isNextTooMuch()) this.scroller.style.marginTop = `${-this.maxOffsetRange()}px`;
-    else {
-      this.scrollerOffset = this.scrollerOffset - this.delta;
-      this.scroller.style.marginTop = `${this.scrollerOffset}px`;
-    }
-  }
-}
+import { Scroller } from "../js/customScroll";
 
 const photoSelectElements = document.querySelectorAll("ol.photos-carrousel > .select");
 const photoListItems = document.querySelectorAll("ol.photos-carrousel > li");
@@ -137,56 +24,49 @@ photoSelectElements.forEach((elem, i) => {
   if (i === 0) elem.click();
 });
 
-const articleMain = document.querySelector("article > main");
-const articleScrollContainer = document.querySelector("article > main > .scroll-container");
-const customScrollbar = document.querySelector("body > .scrollbar");
-const customScrollbarIndicator = document.querySelector("body > .scrollbar > .actual-bar");
-const scrollCeiling = document.querySelector("article > main > .scroll-ceiling");
+const content = document.querySelector("article > main");
+const contentOffsetter = document.querySelector("article > main > .scroll-ceiling");
+const scrollbar = document.querySelector("body > .scrollbar");
+const scrollbarIndicator = document.querySelector("body > .scrollbar > .actual-bar");
+const scroller = new Scroller(content, contentOffsetter, { inverted: false });
 
-/**
- *
- * @param {WheelEvent} event
- * @returns {void}
- */
-function customScrollbarCalculate(event) {
-  if (!customScrollbar || !customScrollbarIndicator || !articleMain || !scrollCeiling) return;
+function scrollbarCalculate() {
+  const totalBarHeight = scroller.scrollable.offsetHeight * 0.4;
+  const contentSize = scroller.scrollable.scrollHeight + Math.abs(scroller.scrollerOffset);
+  scrollbar.style.height = totalBarHeight + "px";
 
-  const scroller = new Scroller(articleMain, scrollCeiling, event?.deltaY ?? 0);
-
-  if (event) {
-    scroller.scroll();
-  }
-
-  if (scroller.scrollerOffset === 0) {
-    customScrollbarIndicator.style.marginTop = "0px";
+  if (scroller.scrollable.clientHeight >= contentSize) {
+    scrollbarIndicator.style.height = totalBarHeight + "px";
+    scrollbarIndicator.style.marginTop = "0px";
     return;
   }
 
-  const percentageScrolled = Math.abs(scroller.scrollerOffset) / scroller.maxOffsetRange();
-  const maxOffsetBarRange = customScrollbar.offsetHeight - customScrollbarIndicator.offsetHeight;
-  customScrollbarIndicator.style.marginTop = `${maxOffsetBarRange * percentageScrolled}px`;
-}
+  scrollbarIndicator.style.height =
+    totalBarHeight * (scroller.scrollable.clientHeight / contentSize) + "px";
 
-function scrollbarHideResizing() {
-  if (!customScrollbar || !customScrollbarIndicator || !articleMain || !scrollCeiling) return;
-  const scroller = new Scroller(articleMain, scrollCeiling, 0);
-  scroller.scroll();
-  const totalBarHeight = articleMain.offsetHeight * 0.4;
-  customScrollbar.style.height = `${totalBarHeight}px`;
-  if (scroller.scrollable.offsetHeight >= scroller.scrollable.scrollHeight) {
-    customScrollbarIndicator.style.height = `${totalBarHeight}px`;
-  } else {
-    customScrollbarIndicator.style.height = `${
-      totalBarHeight * (articleMain.offsetHeight / articleMain.scrollHeight)
-    }px`;
+  if (scroller.scrollerOffset === 0) {
+    scrollbarIndicator.style.marginTop = "0px";
+    return;
   }
-  customScrollbarCalculate();
+
+  // Max possible value of margin-top
+  const barMaxOffsetRange = scrollbar.offsetHeight - scrollbarIndicator.offsetHeight;
+  if (scroller.scroller.style.marginTop === "0px") scrollbarIndicator.style.marginTop = "0px";
+  else if (scroller.scrollerOffset === -scroller.maxOffsetRange)
+    scrollbarIndicator.style.marginTop = barMaxOffsetRange + "px";
+  else
+    scrollbarIndicator.style.marginTop = `${
+      barMaxOffsetRange * (Math.abs(scroller.scrollerOffset) / scroller.maxOffsetRange)
+    }px`;
 }
 
-addEventListener("resize", scrollbarHideResizing);
-articleMain.addEventListener("wheel", customScrollbarCalculate);
+scroller.onscrollend = scrollbarCalculate;
+addEventListener("resize", scrollbarCalculate);
+content.addEventListener("wheel", scroller.onwheel);
+content.addEventListener("touchmove", scroller.ontouchmove);
+content.addEventListener("touchstart", scroller.ontouchstart);
 
-scrollbarHideResizing();
+scrollbarCalculate();
 
 document.querySelectorAll("img[data-storage-origin][data-storage-filename]").forEach((elem) => {
   FetchImage[elem.getAttribute("data-storage-origin")](
---
+++ js/customScroll.js
@@ -0,0 +1,200 @@
+"use strict";
+
+/**
+ * @callback EventCallback
+ * @param {Event} event
+ * @returns {void}
+ */
+
+export class Scroller {
+  #lastTouchYBuffer = null;
+  #deltaBuffer = null;
+
+  /**
+   * @param {Element} scrollable Element that will be scrolled (container)
+   * @param {Element} scroller Element that allows the other to scroll (empty div on top)
+   * @param {{inverted: boolean}} options
+   */
+  constructor(scrollable, scroller, options) {
+    this.scrollable = scrollable;
+    this.scroller = scroller;
+    this.inverted = options?.inverted ?? false;
+    // 0 breaks code cause it means "can't scroll"
+    this.scrollerOffset = 1e-4;
+    this.unset();
+  }
+
+  /**
+   * Which vertical direction does the current offset represent?
+   * @returns {DirectionToken}
+   */
+  vertically() {
+    if (
+      this.delta === 0 ||
+      this.scrollerOffset === this.maxOffsetRange ||
+      this.maxOffsetRange === 0 ||
+      this.scrollerOffset === 0
+    )
+      return Scroller.NOSCROLL;
+    if (this.delta < 0) return Scroller.UP;
+    return Scroller.DOWN;
+  }
+
+  /**
+   * Event listener callback.
+   * @param {TouchEvent} event
+   * @returns {EventCallback}
+   */
+  get ontouchstart() {
+    const proxy = this;
+    return (event) => {
+      if (event.changedTouches.length !== 1) return;
+      proxy.#lastTouchYBuffer = event.changedTouches[0].pageY;
+    };
+  }
+
+  /**
+   * Event listener callback.
+   * @param {TouchEvent} event
+   * @returns {EventCallback}
+   */
+  get ontouchmove() {
+    const proxy = this;
+    return (event) => {
+      if (event.changedTouches.length !== 1) return;
+      proxy.delta = proxy.#lastTouchYBuffer - event.changedTouches[0].pageY;
+      proxy.#lastTouchYBuffer = event.changedTouches[0].pageY;
+      proxy.scroll();
+    };
+  }
+
+  /**
+   * Event listener callback.
+   * @param {WheelEvent} event
+   * @returns {EventCallback}
+   */
+  get onwheel() {
+    const proxy = this;
+    return (event) => {
+      proxy.delta = event.deltaY;
+      proxy.scroll();
+    };
+  }
+
+  /**
+   * Calculate amount of pixels to be additionally offset.
+   * @returns {number}
+   */
+  calc() {
+    return this.scrollerOffset - this.delta;
+  }
+
+  /**
+   * Maximum amount of pixels content can be offset.
+   * @returns {Number}
+   */
+  get maxOffsetRange() {
+    if (this.scrollerOffset < 0) {
+      return (
+        this.scrollable.scrollHeight + Math.abs(this.scrollerOffset) - this.scrollable.offsetHeight
+      );
+    }
+    return this.scrollable.scrollHeight - this.scrollable.offsetHeight;
+  }
+
+  /** @returns {number} */
+  get delta() {
+    if (Object.is(this.#deltaBuffer, null)) return 0;
+    return this.#deltaBuffer;
+  }
+
+  /** @param {number} value  */
+  set delta(value) {
+    if (this.inverted) this.#deltaBuffer = -value;
+    this.#deltaBuffer = value;
+  }
+
+  /**
+   * Either an init operation in case you're inline style isn't already set up, or
+   * a error fallback behavior.
+   */
+  unset() {
+    this.scroller.style.marginTop = "0px";
+  }
+
+  /**
+   * Set scrolling offset.
+   * @param {number} px
+   */
+  set(px) {
+    this.scrollerOffset = px;
+    this.scroller.style.marginTop = px + "px";
+  }
+
+  /**
+   * Will the next scroll put you out of bounds?
+   * @returns {Boolean}
+   */
+  isNextTooMuch() {
+    if (this.vertically() === Scroller.UP && this.calc() > 0) return true;
+    if (this.vertically() === Scroller.DOWN && Math.abs(this.calc()) >= this.maxOffsetRange)
+      return true;
+
+    return false;
+  }
+
+  onscrollend = () => {};
+
+  /**
+   * Scroll content.
+   */
+  scroll() {
+    if (Object.is(this.delta, null)) return;
+    if (this.vertically() === Scroller.NOSCROLL) return;
+
+    if (this.calc() === 0) this.unset();
+    else if (this.isNextTooMuch()) {
+      if (this.vertically() === Scroller.DOWN) this.set(-this.maxOffsetRange);
+      else this.unset();
+    } else this.set(this.calc());
+
+    try {
+      this.onscrollend();
+    } catch (err) {
+      console.error("Specified 'onscrollend' callback is not callable.");
+      console.error(err);
+    }
+
+    this.delta = null;
+  }
+}
+
+/**
+ * My try at unique dummy objects with named types.
+ * Primeagen says its a javascript thing not being able to tell
+ * if an object is unique or something like that. That's why you
+ * can't implement a mapping, says him while talking about algorithms.
+ * TODO Decent research on javascript memory shenanigans, as exemplified above.
+ */
+class DirectionToken extends String {}
+
+Object.defineProperty(Scroller, "UP", {
+  value: new DirectionToken("UP"),
+  writable: false,
+  configurable: false,
+  enumerable: false,
+});
+
+Object.defineProperty(Scroller, "DOWN", {
+  value: new DirectionToken("DOWN"),
+  writable: false,
+  configurable: false,
+  enumerable: false,
+});
+
+Object.defineProperty(Scroller, "NOSCROLL", {
+  value: new DirectionToken("NOSCROLL"),
+  writable: false,
+  configurable: false,
+  enumerable: false,
+});

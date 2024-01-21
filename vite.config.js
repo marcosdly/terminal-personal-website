038619e98a4afb7361a7fdf1d0274cce1a6e1f28
Marcos Delay <dev.marcosdly@gmail.com>
2024-01-20 22:40:05 -0300

    refactor(vite): Logic scalable callback

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,60 @@
-import { defineConfig } from "vite";
+import * as vite from "vite";
 import { ViteMinifyPlugin } from "vite-plugin-minify";
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    port: 6400,
-    open: true,
-  },
-  plugins: [
-    ViteMinifyPlugin({
-      caseSensitive: true,
-      collapseInlineTagWhitespace: false,
-      collapseWhitespace: true,
-      html5: true,
-      keepClosingSlash: true,
-      minifyURLs: true,
-      quoteCharacter: '"',
-      removeComments: true,
-      removeEmptyAttributes: true,
-      removeEmptyElements: false,
-    }),
-  ],
-  css: { devSourcemap: true },
-  logLevel: mode === "development" ? "info" : "silent",
-  clearScreen: false,
-  appType: "mpa",
-  build: {
-    target: "es6",
-    assetsDir: "",
-    assetsInlineLimit: 0,
-    manifest: true,
-    minify: mode === "development" ? "esbuild" : "terser",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: ["index.html", "about/index.html", "./404/index.html"],
-      output: {
-        entryFileNames: "[name]-[hash].js",
-        chunkFileNames: "[name]-[hash].js",
-        assetFileNames: "[name]-[hash][extname]",
+export default vite.defineConfig(({ command, mode }) => {
+  /** @type {vite.UserConfig} */
+  let config = {
+    server: {
+      port: 6400,
+      open: true,
+    },
+    plugins: [
+      ViteMinifyPlugin({
+        caseSensitive: true,
+        collapseInlineTagWhitespace: false,
+        collapseWhitespace: true,
+        html5: true,
+        keepClosingSlash: true,
+        minifyURLs: true,
+        quoteCharacter: '"',
+        removeComments: true,
+        removeEmptyAttributes: true,
+        removeEmptyElements: false,
+      }),
+    ],
+    css: { devSourcemap: true },
+    clearScreen: false,
+    appType: "mpa",
+    build: {
+      target: "es6",
+      assetsDir: "",
+      assetsInlineLimit: 0,
+      manifest: true,
+      emptyOutDir: true,
+      rollupOptions: {
+        input: ["index.html", "about/index.html", "./404/index.html"],
+        output: {
+          entryFileNames: "[name]-[hash].js",
+          chunkFileNames: "[name]-[hash].js",
+          assetFileNames: "[name]-[hash][extname]",
+        },
+      },
+      terserOptions: {
+        format: { comments: false },
       },
     },
-    terserOptions: {
-      format: { comments: false },
-    },
-  },
-  esbuild: { legalComments: "none" },
-}));
+    esbuild: { legalComments: "none" },
+  };
+
+  if (command === "serve" && mode === "development") {
+    config.logLevel = "info";
+    config.build.minify = "esbuild";
+  }
+
+  if (mode === "production") {
+    config.logLevel = "silent";
+    build.minify = "terser";
+  }
+
+  return config;
+});

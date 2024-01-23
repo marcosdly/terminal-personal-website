8241ee9915b86bc8873af32f2a370ca1c8446e06
Marcos Delay <dev.marcosdly@gmail.com>
2024-01-23 04:18:25 -0300

    refactor(homepage): Page selector's animation

--- index/animations.js
+++ index/animations.js
@@ -1,5 +1,108 @@
 "use strict";
 
+/**
+ * @callback EventCallback
+ * @param {Even|undefined} event
+ * @returns {void}
+ */
+
+class NavLink {
+  clicked = false;
+  hovering = false;
+  static anyClicked = false;
+
+  /**
+   * @param {Element} element Main nav's link element
+   */
+  constructor(element) {
+    this.elem = element;
+    this.text = this.elem.getAttribute("originaltext");
+    this.sans = this.elem.querySelector(".font-sans");
+    this.mono = this.elem.querySelector(".font-mono");
+    this.paren = this.elem.querySelector(".parenthesis");
+    this.colorClass = this.elem.getAttribute("colorclass");
+  }
+
+  async runEventLoop() {
+    this.elem.addEventListener("mouseover", this.onmouseover);
+    this.elem.addEventListener("mouseout", this.onmouseout);
+    this.elem.addEventListener("click", this.onclick);
+
+    while (true) {
+      if (NavLink.anyClicked && !this.clicked) {
+        this.elem.classList.add("main-nav-link-hide");
+        return;
+      }
+      this.animate();
+      await new Promise((resolve) => setTimeout(resolve, 100));
+    }
+  }
+
+  async animate() {
+    // FIX Animation can start before nickname's animation end
+    const sans = this.sans.textContent,
+      mono = this.mono.textContent,
+      paren = this.paren.textContent,
+      formatted = this.text.replaceAll(" ", "_");
+
+    if (this.clicked && mono === this.originalText && paren === "()") return;
+
+    if (this.hovering || this.clicked) {
+      if (paren === "()") return;
+      if (paren === "" && mono === formatted) this.mono.classList.add(this.colorClass);
+      if (mono === formatted) {
+        this.paren.textContent += paren === "(" ? ")" : "(";
+        return;
+      }
+      this.mono.textContent += sans[0] === " " ? "_" : sans[0];
+      this.sans.textContent = sans.substring(1);
+      return;
+    }
+
+    if (paren != "") {
+      this.paren.textContent = paren.substring(0, paren.length - 1);
+      return;
+    }
+    if (paren === "" && mono === formatted) this.mono.classList.remove(this.colorClass);
+    if (mono === "") return;
+    const lastChar = mono.at(-1) === "_" ? " " : mono.at(-1);
+    this.sans.textContent = lastChar + this.sans.textContent;
+    this.mono.textContent = mono.substring(0, mono.length - 1);
+  }
+
+  /** @returns {EventCallback} */
+  get onmouseover() {
+    const proxy = this;
+    return () => {
+      proxy.hovering = true;
+    };
+  }
+
+  /** @returns {EventCallback} */
+  get onmouseout() {
+    const proxy = this;
+    return () => {
+      proxy.hovering = false;
+    };
+  }
+
+  /** @returns {EventCallback} */
+  get onclick() {
+    const proxy = this;
+    return (event) => {
+      event.preventDefault();
+      proxy.clicked = true;
+      NavLink.anyClicked = true;
+
+      document.querySelector(".terminal-animation-build-target").textContent =
+        proxy.elem.getAttribute("originaltext").toLowerCase().replaceAll(" ", "_");
+
+      document.querySelector(".terminal").classList.add("terminal-animate");
+      this.elem.classList.add("animate");
+    };
+  }
+}
+
 function animateNickname() {
   document.querySelector(".nickname").style["animation-play-state"] = "running";
   document.querySelector(".main-header").style["animation-play-state"] = "running";
@@ -38,54 +141,6 @@ class NavLinkAnimationState {
   static elementClicked = null;
 }
 
-/**
- * @param {Element} elem
- * @param {NavLinkAnimationState} state
- */
-async function animateMainNavLink(elem, state) {
-  // TODO cover links their animation wont start until nickname animation ends
-  const sansText = state.sans.textContent,
-    monoText = state.mono.textContent,
-    parenthesisText = state.parenthesis.textContent,
-    mouseIsOver = elem.getAttribute("mouseisover"),
-    originalText = elem.getAttribute("originaltext"),
-    originalTextFormatted = originalText.replaceAll(" ", "_"),
-    colorClass = elem.getAttribute("colorclass"),
-    wasClicked = elem.getAttribute("wasclicked");
-
-  if (wasClicked === "true" && monoText === originalText && parenthesisText === "()") {
-    NavLinkAnimationState.keepRunning = false;
-    return;
-  }
-
-  if (mouseIsOver === "true" || wasClicked === "true") {
-    if (parenthesisText.endsWith(")")) return;
-    if (parenthesisText === "" && monoText === originalTextFormatted)
-      state.mono.classList.add(colorClass);
-    if (monoText === originalTextFormatted) {
-      state.parenthesis.textContent += parenthesisText === "(" ? ")" : "(";
-      return;
-    }
-    state.mono.textContent += sansText.at(0) === " " ? "_" : sansText.at(0);
-    state.sans.textContent = sansText.substring(1);
-    return;
-  }
-
-  if (parenthesisText != "") {
-    state.parenthesis.textContent = parenthesisText.substring(
-      0,
-      parenthesisText.length - 1,
-    );
-    return;
-  }
-  if (parenthesisText === "" && monoText === originalTextFormatted)
-    state.mono.classList.remove(colorClass);
-  if (monoText === "") return;
-  const lastChar = monoText.at(-1) === "_" ? " " : monoText.at(-1);
-  state.sans.textContent = lastChar + state.sans.textContent;
-  state.mono.textContent = monoText.substring(0, monoText.length - 1);
-}
-
 async function animateTerminalCommandText() {
   const commandElement = document.querySelector(".terminal-cli-command");
   commandElement.style.display = "block";
@@ -117,46 +172,8 @@ async function animateTerminalText(text) {
     window.location.assign(NavLinkAnimationState.elementClicked.href);
 }
 
-const mainNavLinks = document.querySelectorAll(".main-nav a");
-
-for (const elem of mainNavLinks) {
-  (async function () {
-    const state = new NavLinkAnimationState(elem);
-    while (true) {
-      if (!NavLinkAnimationState.keepRunning) break;
-      await animateMainNavLink(elem, state);
-      await new Promise((resolve) => setTimeout(resolve, 100));
-    }
-  })();
-
-  elem.addEventListener("mouseover", async (event) => {
-    event.currentTarget.setAttribute("mouseisover", "true");
-  });
-
-  elem.addEventListener("mouseout", async (event) => {
-    event.currentTarget.setAttribute("mouseisover", "false");
-  });
-
-  elem.addEventListener("click", async (event) => {
-    event.preventDefault();
-    event.currentTarget.setAttribute("wasclicked", "true");
-    NavLinkAnimationState.elementClicked = event.currentTarget;
-    document.querySelector(".terminal-animation-build-target").textContent =
-      event.currentTarget
-        .getAttribute("originalText")
-        .toLowerCase()
-        .replaceAll(" ", "_");
-
-    const intersection = [];
-    mainNavLinks.forEach((x) => {
-      if (!Object.is(x, elem)) intersection.push(x);
-    });
-
-    for (const el of intersection) el.classList.add("main-nav-link-hide");
-    document.querySelector(".terminal").classList.add("terminal-animate");
-    elem.style["animation-play-state"] = "running";
-  });
-}
+for (const elem of document.querySelectorAll(".main-nav a"))
+  new NavLink(elem).runEventLoop();
 
 addEventListener("animationend", async (event) => {
   if (event.animationName === "main-nav-link-hide") {
